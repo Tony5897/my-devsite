227fc9073eaa18be5ba9ed1ecdf74d354f4a5efb
Tony5897 <tony.martinez5897@gmail.com>
2023-02-12 11:42:08 -0800

    final commit before launch

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,35 +1,42 @@
-// Nav hamburgermenu 
-
+// Nav hamburgermenu selections
 const burger = document.querySelector("#burger-menu");
 const ul = document.querySelector("nav ul");
 const nav = document.querySelector("nav");
 
-// Scroll to Top
+//CSS Animate on Scroll fuctionality
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    // console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+        entry.target.classList.remove('show');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+// Scroll to Top Section
 const scrollUp = document.querySelector("#scroll-up");
 
 // Select nav links
-const navLink = document.querySelector("#nav-link");
+const navLink = document.querySelectorAll(".nav-link");
 
-// hamburger menu functionality
+// hamburger menu functionality 
+burger.addEventListener("click", () => {
+  ul.classList.toggle("show");
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Hamburger menu close upon click
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    ul.classList.remove("show");
+  })
+);  
 
 // Scroll to Top Functionality
-
 scrollUp.addEventListener("click", () => {
   window.scrollTo({
     top: 0,
